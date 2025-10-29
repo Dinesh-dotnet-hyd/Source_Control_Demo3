5bc5c01e9562ca23b5d148857723900640380eac
Boda Praveen <Boda.Praveen@encora.com>
2025-10-29 15:39:52 +0530

    first commit from hyd branch

---
+++ Source_Control_Demo3/Hyderabad.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Source_Control_Demo3
+{
+    internal class Hyderabad
+    {
+        public Hyderabad() 
+            {
+            Console.WriteLine("Hyderabad Branch");
+            }  
+    }
+}
